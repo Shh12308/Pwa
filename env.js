d3953081dff96b0409e5aee76694df370665cadb
Shh12308 <164091255+Shh12308@users.noreply.github.com>
2026-04-21 15:29:22 +0100

    Update env.js

--- env.js
+++ env.js
@@ -2,7 +2,7 @@ window.ENV = {
   SUPABASE_URL: "https://xmmqhahsfhlmplbuchat.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhtbXFoYWhzZmhsbXBsYnVjaGF0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYxNDQxMTQsImV4cCI6MjA4MTcyMDExNH0.vHIu1A2uMw4d9eFFyOj98lvVOppC8msdMCbS1Oj-lAM",
 
-  FREE_AI_URL: "https://boxsw3vzyj5afc-8000.proxy.runpod.net",
+  FREE_AI_URL: "https://xrahe1hg5aanqr-8080.proxy.runpod.net",
   PREMIUM_AI_URL: "https://free.com",
 
   RECAPTCHA_SITE_KEY: "6Lc1EoQsAAAAALCFeitfMPlBZFMO6XlKL6TwEVv5",
